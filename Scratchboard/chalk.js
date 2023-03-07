dea2f6c26b312beedb5b2b1e4bc9f455cca95b6a
Myles DeBoer <myles.deboer@gmail.com>
2023-03-07 08:27:29 -0800

    scratchboard test

--- Scratchboard/chalk.js
+++ Scratchboard/chalk.js
@@ -1,1 +1,6 @@
 // SCRATCHBOARD - write, erase, or don't erase and come back to it later. 
+
+let test = ['a','b','c']
+let chooseAndRepeat = test[0].repeat(3) + ": oh cool, some letters"
+console.log(chooseAndRepeat);
+
