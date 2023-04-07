cc6a2fa5187c03edc5fc8e98157591e2f83a4739
Myles DeBoer <myles.deboer@gmail.com>
2023-04-06 18:17:51 -0700

    nesting desctructuring mock ups

--- Scratchboard/chalk.js
+++ Scratchboard/chalk.js
@@ -1,6 +1,21 @@
 // SCRATCHBOARD - write, erase, or don't erase and come back to it later. 
 
-let test = ['a','b','c']
-let chooseAndRepeat = test[0].repeat(3) + ": oh cool, some letters"
-console.log(chooseAndRepeat);
+const array = [1,2,3,4,5]
+const object = {
+  myName: 'Myles',
+  age: 29,
+  eyes: "blue",
+  hobbies: {
+    hobby1: "ski",
+    hobby2: "bike",
+    hobby3: "cook"
+  }
+}
 
+const [one, two] = array
+const {myName, age } = object
+const {eyes, hobbies: {hobby1, hobby2}} = object
+
+console.log(one, two);
+console.log(myName, age);
+console.log(eyes, hobby1, hobby2);
